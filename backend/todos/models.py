f1ee86112616dd151d71c5e62be143c2e0f5c2cb
Hourann <tdkgn@outlook.com>
2018-09-10 17:25:14 +0800

    enable django admin

--- backend/todos/models.py
+++ backend/todos/models.py
@@ -4,11 +4,11 @@ from django.db import models
 # Create your models here.
 class Todo(models.Model):
     # mandatory
-    title = models.CharField(max_length=200)
-    description = models.TextField()
+    title = models.CharField(max_length=200, null=False)
+    description = models.TextField(null=False)
 
     # optional
-    expire_date = models.DateTimeField()
+    expire_date = models.DateTimeField(null=True)
     priority = models.CharField(max_length=1, choices=(('H', 'High'), ('N', 'Normal')), default='N', null=False)
 
     def __str__(self):
